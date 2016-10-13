8eb801dc17a561de8138d53ae4fb9056b6b55553
geoffshaw <geoffshawbc@gmail.com>
2016-10-13 13:37:06 -0700

    Moves client and server code into their own module

---
+++ client.py
@@ -0,0 +1,11 @@
+import socket
+
+def run(TCP_IP, TCP_PORT, BUFFER_SIZE):
+    MESSAGE = "Hello, World!"
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((TCP_IP, TCP_PORT))
+    s.send(MESSAGE)
+    data = s.recv(BUFFER_SIZE)
+    s.close()
+
+    print "received data:", data
--- vpn.py
+++ vpn.py
@@ -1,35 +1,14 @@
 #!/usr/bin/env python
 
 import socket
+import server
+import client
 
 
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5005
 BUFFER_SIZE = 1024
 MESSAGE = "Hello, World!"
-
-def client():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((TCP_IP, TCP_PORT))
-    s.send(MESSAGE)
-    data = s.recv(BUFFER_SIZE)
-    s.close()
-
-    print "received data:", data
-
-def server():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((TCP_IP, TCP_PORT))
-    s.listen(1)
-
-    conn, addr = s.accept()
-    print 'Connection address:', addr
-    while 1:
-        data = conn.recv(BUFFER_SIZE)
-        if not data: break
-        print "received data:", data
-        conn.send(data)  # echo
-    conn.close()
 
 def main():
     cliServ = 9999
@@ -42,9 +21,9 @@ def main():
         print "Please enter 0 if this is the server and 1 if this is the client"
 
     if cliServ == 0:
-        server()
+        server.run(TCP_IP, TCP_PORT, BUFFER_SIZE)
     else:
-        client()
+        client.run(TCP_IP, TCP_PORT, BUFFER_SIZE)
 main()
 
 
